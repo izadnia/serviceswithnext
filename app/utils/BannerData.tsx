5b70c0ca417351139216a712044103e098bc5060
Afshin Izadnia <izadnia@yahoo.com>
2023-09-20 20:48:46 +0430

    .

--- app/utils/BannerData.tsx
+++ app/utils/BannerData.tsx
@@ -4,13 +4,13 @@ export const BannerData = [
         timeOfOffer:"Todays Special",
         description:"Get discount for every order, only valid for today",
         color:"bg-blue-400",
-        image:"/images/Banner/1.png"
+        image:"/Images/Banner/1.png"
 },
 {
     title:"80%",
     timeOfOffer:"This Weekends",
     description:"Get discount for every order, only valid for today",
     color:"bg-green-400",
-    image:"/images/Banner/1.png"
+    image:"/Images/Banner/1.png"
 }
 ]
